5c4866f530335898f8505c694a8b587ec74b98e7
CJCU-CC <108b32079@mailst.cjcu.edu.tw>
2021-03-30 11:57:40 +0800

    0330

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from mysite import models
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pmodel', 'nickname', 'price', 'year')
+    search_fields = ('nickname',)
+    ordering = ('-price',)
+
+admin.site.register(models.Maker)
+admin.site.register(models.PModel)
+admin.site.register(models.Product, ProductAdmin)
+admin.site.register(models.PPhoto)
--- mysite/models.py
+++ mysite/models.py
@@ -19,11 +19,11 @@ class PModel(models.Model):
 
 
 class Product(models.Model):
-    pmodel = models.ForeignKey(PModel, on_delete=models.CASCADE)
-    nickname = models.CharField(max_length=15, default='超值二手機')
+    pmodel = models.ForeignKey(PModel, on_delete=models.CASCADE, verbose_name='型號')
+    nickname = models.CharField(max_length=15, default='超值二手機', verbose_name='摘要')
     description = models.TextField(default='暫無說明')
-    year = models.PositiveIntegerField(default=2016)
-    price = models.PositiveIntegerField(default=0)
+    year = models.PositiveIntegerField(default=2016, verbose_name='出廠年份')
+    price = models.PositiveIntegerField(default=0, verbose_name='價格')
 
     def __str__(self):
         return self.nickname
